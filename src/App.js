6fbc2f3a130e292ad7b3f2f379de0adf6ea3286b
eunjeongsong <hellogine12@gmail.com>
2018-11-22 00:43:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,61 +1,67 @@
-import React, { Component } from 'react';
-import PhoneForm from './components/PhoneForm';
-import PhoneInfoList from './components/PhoneInfoList';
+import React, { Component } from "react";
+import "./App.css";
+import MyComponent from "./MyComponent";
 
 class App extends Component {
-
-  id = 0;
-
   state = {
-    information: [],
-  }
+    counter: 1,
+    hasError: false
+  };
 
-  handleCreate = (data) => {
-    const { information } = this.state;
+  componentDidCatch = (error, info) => {
     this.setState({
-      information: information.concat(
-        Object.assign({}, data, {
-          id: this.id++,
-        })
-      ),
+      hasError: true
     });
+  };
+
+  /**
+   *   컴포넌트가 처음 브라우저에 나타나게 될때 가장 먼저 실행되는 함수.
+   *   - 컴포넌트가 가지고 있어야 하는 state 등 미리해야하는 작업들을 설정.
+   */
+  constructor(props) {
+    super(props);
+    console.log("constructor");
   }
 
-  handleRemove = (id) => {
-    const { information } = this.state;
+  /**
+   *    컴포넌트가 브라우저에 나타난 이후 시점에 뭔가를 하겠다.
+   *    - 라이브러리 (차트)
+   *    - GraphQL
+   *    - Ajax 요청
+   *    - 컴포넌트가 나타난 다음 DOM 에서 스크롤 이벤트를 읽고싶다.
+   *    - 등등...
+   */
+  componentDidMount() {
+    //   ajax, GraphQL 등의 작업 수행...
+    console.log("componentDidMount");
+    console.log(this.myDiv.getBoundingClientRect());
+  }
+
+  handleClick = () => {
     this.setState({
-      information: information.filter(info => info.id !== id)
+      counter: this.state.counter + 1
     });
-  }
-
-  handleUpdate = (id, data) => {
-    const { information } = this.state;
-    this.setState({
-      information: information.map(
-        info => {
-          if(info.id === id){
-            return {
-              id,
-              ...data,
-            };
-          }
-          return info; 
-        }
-      )
-    })
-  }
+  };
 
   render() {
-    return (
-      <div>
-        <PhoneForm onCreate={this.handleCreate}/>
-        <PhoneInfoList 
-          data={this.state.information}
-          onRemove={this.handleRemove}
-          onUpdate={this.handleUpdate}
-        />
-      </div>
-    );
+    if (this.state.hasError) {
+      return <div>Error 가 발생했어요...</div>;
+    } else {
+      return (
+        <div ref={ref => (this.myDiv = ref)}>
+          {/* 
+            Prpps 는 부모 컴포넌트가 자식 컴포넌트에게 값을 전달할 때 사용 
+            <Child value="value"/>    
+        */}
+          {/* 멀티 라인 주석 작성법 */}
+          {/* this.state.counter 가 10 이 넘게되면 MyComponent 가 사라지고, 사라질때 componentWillUnmount 호출 */}
+          {this.state.counter < 10 && (
+            <MyComponent value={this.state.counter} />
+          )}
+          <button onClick={this.handleClick}>Click me!</button>
+        </div>
+      );
+    }
   }
 }
 
